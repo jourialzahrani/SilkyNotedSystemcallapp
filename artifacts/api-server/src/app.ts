c58c0c798e46c5b0ab607295f10bbaeba86b5a6b
jourialzahrani <jourisaeed44@gmail.com>
2026-09-01 14:11:36 +0300

    Fix Vercel build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp = require("pino-http");
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,21 +10,18 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+      req: (req) => ({
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      }),
+      res: (res) => ({
+        statusCode: res.statusCode,
+      }),
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
